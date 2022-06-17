5f25a6eecea615b8320b39f2209f878c1ebfdc1f
Paige Lawson <paigelawson1999@gmail.com>
2022-06-18 00:54:24 +1000

    Trying my best

--- README.md
+++ README.md
@@ -5,11 +5,16 @@
 * HTML
 * CSS
 
+### Aproach
+* Tried to keep it simple while looking decent.
 
-### Things to Improve
+#### Things to Improve
 * Sumbit onkey press
 * Validation on letters (can be empty on screen and grey tiles come up)
 * Make the tile flipping or any animations or sounds
+* Use API for words
+* 
+
 
 
 
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const wordList =  ["lines","peach","salty","ghost","apron",]
 const answer = wordList[Math.floor(Math.random() * wordList.length)]
 let counter = 0;
-// let keys = document.getElementByClassName("")
+
 
  //choses word from list above (array)
 function abc(){
@@ -24,7 +24,7 @@ function abc(){
 }
 function wordle(str){
 
-  console.log(answer)
+
 for ( let i = 0; i < answer.length; i ++) {
    let value = answer.indexOf(str[i])
    const letterId = getfirstLetterId(counter) + i;
@@ -56,10 +56,10 @@ function createSquares() {
        let square = document.createElement("div");
        square.classList.add("square");
        square.setAttribute("id", index);
-        square.setAttribute("contenteditable", "true")
-        square.setAttribute("onkeypress","return (this.innerText.length <= 0)")
+        square.setAttribute("contenteditable", "true") //lets me click on the boxes
+        square.setAttribute("onkeypress","return (this.innerText.length <= 0)") //Type1letter
         square.setAttribute("max","1")
       gameBoard.appendChild(square);
-      if ((index + 1) % 5 ==0) {groupCount ++} //
+      if ((index + 1) % 5 ==0) {groupCount ++} 
     }
    }
